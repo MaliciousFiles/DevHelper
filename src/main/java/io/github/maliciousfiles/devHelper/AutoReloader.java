c540130106fc32c118300682c46c285b0c8f61a9
Malcolm Roalson <mtroalson@gmail.com>
2026-05-09 04:02:30 -0500

    update to mc 26.2

--- src/main/java/io/github/maliciousfiles/devHelper/AutoReloader.java
+++ src/main/java/io/github/maliciousfiles/devHelper/AutoReloader.java
@@ -6,7 +6,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.PluginCommand;
 import org.bukkit.command.SimpleCommandMap;
-import org.bukkit.craftbukkit.v1_21_R1.CraftServer;
+import org.bukkit.craftbukkit.CraftServer;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.*;
